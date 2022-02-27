bf681351de71ed5b2d6309339ad601bcb6797944
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-02-27 21:57:58 +1100

    created react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './Components/NavBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+        {/* Login */}
+        {/* Feed */}
+            {/* FeedItem */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='nav__bar'>
+        <h1 className='nav__bar--logo'>LinesNearMe</h1>
+    </div>
+  )
+}
+
+export default NavBar
